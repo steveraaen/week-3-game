80f4bf77b66c19677cfe01dd654c99bfe4cb9ba6
steveraaen <sraaen@gmail.com>
2016-11-18 16:20:31 -0500

    v.2

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,51 +1,39 @@
-var stfull = "src = 'assets/images/hangmanfull.png' alt='full stickman figure'";
-var st1 = "src = 'assets/images/hangman-1.png' alt='full stickman figure'";
-var st2 = "src = 'assets/images/hangman-2.png' alt='full stickman figure'";
-var st3 = "src = 'assets/images/hangman-3.png' alt='full stickman figure'";
-var st4 = "src = 'assets/images/hangman-4.png' alt='full stickman figure'";
-var st5 = "src = 'assets/images/hangman-5.png' alt='full stickman figure'";
-var stlose = "src = 'assets/images/hangman-lose.png' alt='full stickman figure'";
-var stcurrent = "src = 'assets/images/hangmanfull.png' alt='full stickman figure'"
+var ans = "nirvana",
+	gues = "_______",
+	a = document.getElementById("input");
+	spans = ans.split("");
+	spgues = gues.split("")
 
-var strng= "nirvana"
-var newstrng= [];
-var i = 0;
-;
-	while (newstrng.length != strng.length){
-		var guess = prompt("Guess a letter")
-			if (guess = strng[i])	{
-				newstrng.push(strng[i])
-			}	
-			else prompt("Guess Again")
-	}
+console.log(spgues);
+console.log(spans);
 
 
+      // Next, we give JavaScript a function to execute when onkeyup event fires.
+
+input.onkeyup = function () {
+    a.value = event.key;
+    	for (i = 0; i < spans.length; i++)	{
+    		if (spans[i] === a.value)	{
+    			var n = spans.indexOf(spans[i]);
+    			spgues.splice(a.value,1, spans[n]);
+
+console.log(n);    		
+console.log(a.value);
+console.log(spans[i])
+console.log(spans[i] === a.value)
+    	}
+    }
+/*	    var n = spans.indexOf(a);*/
+/*console.log(a.value);
+console.log(spans[i])	*/	
+
+/*console.log(a.value);
+console.log(input.value);
+console.log(i);*/
+var remaining = document.getElementById("usedletters");
+remaining.innerHTML = spgues;
+}
 
 
-
-function stick(){
-	cp = document.getElementById("left")
-		var count = 6;
-		var let = prompt("Guess A Letter");
-			while (count > 0){
-				if (count === 5){
-					stcurrent = "src = 'assets/images/hangman-1.png' alt='full stickman figure'";
-				}
-				else if (count === 4){
-					stcurrent = "src = 'assets/images/hangman-2.png' alt='full stickman figure'";
-				}
-				else if (count === 3){
-					stcurrent = "src = 'assets/images/hangman-3.png' alt='full stickman figure'";
-				}
-				else if (count === 2){
-					stcurrent = "src = 'assets/images/hangman-4.png' alt='full stickman figure'";
-				}
-				else if (count === 1){
-					stcurrent = "src = 'assets/images/hangman-5.png' alt='full stickman figure'";
-				}
-				else{
-					var stcurrent = "src = 'assets/images/hangman-lose.png' alt='full stickman figure'";
-				}
-		count -= 1;
-	}
-	}
+var remaining = document.getElementById("usedletters");
+remaining.innerHTML = spgues;
